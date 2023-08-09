3e2ae1986fe0c243e93364e3f69e7c61a35f69a2
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-08-09 07:28:26 +0700

    Add LRU cache information

---
+++ python/lru-cache-info.py
@@ -0,0 +1,23 @@
+# LRU Cache Info
+
+You can add the `lru_cache` decorator to a function:
+
+```python
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+```
+
+To retrieve cache information (e.g. hits, misses, current size), use the `cache_info` method:
+
+```python
+>>> [fib(n) for n in range(16)]
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+
+>>> fib.cache_info()
+CacheInfo(hits=28, misses=16, maxsize=None, currsize=16)
+```
